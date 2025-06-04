70a039e6b34a36a0f5def5a57c923026a96c682b
samehere7 <a123110010@gmail.com>
2025-06-04 18:04:04 +0800

    部署报错

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,26 @@
-import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { LanguageProvider } from "@/hooks/use-language"
-import Script from "next/script"
+import { LanguageProvider } from "@/components/language-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Maoge PDF",
-  description: "AI-powered PDF analysis and chat. Upload, organize, and chat with your PDFs easily.",
-  generator: 'v0.dev'
+  title: "maoge pdf",
+  description: "maoge pdf",
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="zh">
-      <body className={`${inter.className} bg-gray-50 min-h-screen text-gray-900`}> 
+    <html lang="en">
+      <body className={inter.className}>
         <LanguageProvider>
           {children}
         </LanguageProvider>
-        <Script id="clarity-script" strategy="afterInteractive">{
-          `
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "ru3ef5mdsl");
-          `
-        }</Script>
-        <Script defer data-domain="maogepdf.com" src="https://plausible.io/js/script.js" strategy="afterInteractive" />
       </body>
     </html>
   )
