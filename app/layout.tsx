07f4a1f833ca373756abcbb5570c6cac937c84c9
samehere7 <a123110010@gmail.com>
2025-06-04 14:35:22 +0800

    feat: 集成 Microsoft Clarity 统计脚本

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <LanguageProvider>
           {children}
         </LanguageProvider>
+        <Script id="clarity-script" strategy="afterInteractive">{
+          `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ru3ef5mdsl");
+          `
+        }</Script>
         <Script defer data-domain="maogepdf.com" src="https://plausible.io/js/script.js" strategy="afterInteractive" />
       </body>
     </html>
